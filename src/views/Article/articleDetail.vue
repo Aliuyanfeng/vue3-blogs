5f84cb6324c426193a6948b3e57126a972ff6c5f
aliuyanfeng <1773189480@qq.com>
2022-06-01 15:02:58 +0800

    feat:pdf 打印

--- src/views/Article/articleDetail.vue
+++ src/views/Article/articleDetail.vue
@@ -5,22 +5,30 @@
 
   <main class="blog-main">
     <a-space direction="vertical" style="width: 100%">
-      <a-page-header
-        style="border: 1px solid rgb(235, 237, 240)"
-        :title="articleInfo.data.article_title"
-        :breadcrumb="{ routes }"
-        :ghost="false"
-      >
-        <template #tags>
-          <a-tag
-            :color="item['theme_color']"
-            v-for="(item, index) in dealTag"
-            :key="index"
-            >{{ item["classify_name"] }}</a-tag
-          >
-        </template>
-      </a-page-header>
-      <a-card :loading="loading" :title="articleInfo.data.article_description">
+      <div class="page-header-wrap">
+        <a-page-header
+          style="border: 1px solid rgb(235, 237, 240)"
+          :title="articleInfo.data.article_title"
+          :breadcrumb="{ routes }"
+          :ghost="false"
+        >
+          <template #tags>
+            <a-tag
+              :color="item['theme_color']"
+              v-for="(item, index) in dealTag"
+              :key="index"
+              >{{ item["classify_name"] }}</a-tag
+            >
+          </template>
+        </a-page-header>
+        <a-tooltip placement="top">
+          <template #title>
+            <span>打印为PDF</span>
+          </template>
+          <FilePdfOutlined class="pdf-print" @click="printPdf"/>
+        </a-tooltip>
+      </div>
+      <a-card id="printcontent" :loading="loading" :title="articleInfo.data.article_description">
         <div class="markdown-body" v-html="articleInfo.data.article_content"></div>
       </a-card>
     </a-space>
@@ -44,6 +52,8 @@ import { getAllTag } from "../../api/index";
 import { useStore } from "../../store";
 
 import { useRoute } from "vue-router";
+
+import { FilePdfOutlined } from "@ant-design/icons-vue"
 
 const store = useStore();
 
@@ -100,6 +110,31 @@ const routes = [
     breadcrumbName: "文章详情",
   },
 ];
+
+// 输出局部pdf
+const printPdf = () =>{
+  //判断iframe是否存在，不存在则创建iframe
+  var iframe:any  = document.getElementById("print-iframe");
+  if(!iframe){  
+      var el:any  = document.getElementById("printcontent");
+      iframe = document.createElement('IFRAME');
+      var doc = null;
+      iframe.setAttribute("id", "print-iframe");
+      iframe.setAttribute('style', 'position:absolute;width:0px;height:0px;left:-500px;top:-500px;');
+      document.body.appendChild(iframe);
+      doc = iframe.contentWindow.document;
+      //这里可以自定义样式
+      //doc.write("<LINK rel="stylesheet" type="text/css" href="css/print.css">");
+      doc.write(`<title>${articleInfo.data.article_title}</title>`);
+      doc.write('<div>' + el.innerHTML + '</div>');
+      doc.close();
+      iframe.contentWindow.focus();            
+  }
+  iframe.contentWindow.print();
+  if (navigator.userAgent.indexOf("MSIE") > 0){
+      document.body.removeChild(iframe);
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -113,6 +148,18 @@ main {
     padding: 15px;
     h1, h2, h3, h4, h5, h6{
       color: inherit;
+    }
+  }
+
+  .page-header-wrap {
+    position: relative;
+    .pdf-print {
+      font-size: 30px;
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      right: 20px;
+      cursor: pointer;
     }
   }
 }
